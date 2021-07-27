d8e2b410536620f5439cc5a9f1434db5289c8ce4
panchen <panchen@tigerbrokers.com>
2021-07-27 17:27:25 +0800

    CONNECTIVITY_SERVICE not work since N in AndroidManifest.

--- app/src/main/java/me/bytebeats/sentry/SentryApplication.kt
+++ app/src/main/java/me/bytebeats/sentry/SentryApplication.kt
@@ -1,8 +1,12 @@
 package me.bytebeats.sentry
 
+import android.app.ActivityManager
 import android.app.Application
 import android.content.Context
 import android.content.res.Configuration
+import android.text.TextUtils
+import me.bytebeats.sentry.network.NetworkChangeListenHelper
+
 
 /**
  * Created by bytebeats on 2021/6/5 : 17:21
@@ -10,12 +14,18 @@ import android.content.res.Configuration
  * Quote: Peasant. Educated. Worker
  */
 class SentryApplication : Application() {
+
+    private var mNetworkChangeListenHelper: NetworkChangeListenHelper? = null
+
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
     }
 
     override fun onCreate() {
         super.onCreate()
+        if (isAppMainProcess()) {
+            registerNetworkListener()
+        }
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
@@ -32,5 +42,51 @@ class SentryApplication : Application() {
 
     override fun onTerminate() {
         super.onTerminate()
+    }
+
+    /**
+     * 监听网络变化
+     */
+    private fun registerNetworkListener() {
+        if (mNetworkChangeListenHelper?.hasRegisterNetworkCallback() == true) {
+            return
+        }
+        mNetworkChangeListenHelper = NetworkChangeListenHelper()
+        mNetworkChangeListenHelper?.registerNetworkCallback(this, object :
+            NetworkChangeListenHelper.NetworkChangeListener {
+            override fun onNetworkChange(isNetworkAvailable: Boolean) {
+                if (!isNetworkAvailable) {
+                    return
+                }
+            }
+        })
+    }
+
+    /**
+     * Application.onCreate 在多进程时会被调用多次.
+     * 为了使一些初始化操作只能在主进程中进行, 特使用该方法来区分进程
+     * @return
+     */
+    protected fun isAppMainProcess(): Boolean {
+        return try {
+            val pid: Int = android.os.Process.myPid()
+            val process = getProcessNameByPid(pid)
+            !TextUtils.isEmpty(process) && TextUtils.equals(packageName, process)
+        } catch (e: Exception) {
+            true
+        }
+    }
+
+    private fun getProcessNameByPid(pid: Int): String {
+        val manager = getSystemService(ACTIVITY_SERVICE) as ActivityManager
+        if (manager == null || manager.runningAppProcesses == null) {
+            return ""
+        }
+        for (runningAppProcess in manager.runningAppProcesses) {
+            if (runningAppProcess.pid == pid) {
+                return runningAppProcess.processName
+            }
+        }
+        return ""
     }
 }
